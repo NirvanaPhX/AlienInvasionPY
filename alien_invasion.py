ad25e6f9e212cba0a45bb9e262c3315841d326a4
Nirvanaphx <lixiaomengg@gmail.com>
2020-11-29 09:08:25 -0700

    Added function to implement ship movement (Limit the movement inside the screen, Ability to control the ship speed)

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,30 +4,64 @@ import pygame
 from settings import Settings
 from ship import Ship
 
-class AlienInvasion:
 
+class AlienInvasion:
+    """Main Game Class, contains main game loop and other game actions"""
 
     def __init__(self):
+        """Initialize the game using Pygame built-in functions"""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        """Full screen implementation"""
+        """
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        """
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
 
     def run_game(self):
-
+        """Function to start the Main game loop"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Function to check and manage game behavior"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
-            pygame.display.flip()
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_Q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Function to draw the screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -1,17 +1,36 @@
 import pygame
 
 class Ship:
+    """A class to manage the ship"""
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
 
+        # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-
+        # Initialize starting position
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
+        # Set up movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """Function to manage the ship action"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
+        """Draw ship to the screen"""
         self.screen.blit(self.image, self.rect)
